655b5c875006f1d87ea78cacd3757de65a12147c
luiz <luizfernandoklein@live.com>
2020-04-15 19:41:22 -0300

    Resolved tests

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,17 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  balance: {
+    income: number;
+    outcome: number;
+    total: number;
+  };
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +19,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type, balance }: Request): Transaction {
+    if (type === 'outcome' && balance.total < value) {
+      throw Error(
+        'The transaction have a value bigger than your total balance',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
